c2f98a67965104bbe4775e3c8a21e888b76475f5
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-08-28 22:15:22 -0400

    created Comment model and created serializer for comment app

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
